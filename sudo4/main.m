8ad1601cff11718f71193c95e257766b90120364
Alun You <alun@Aluns-MacBook-Pro.local>
2013-07-03 00:05:46 +0800

    Initial Commit

---
+++ sudo4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  sudo4
+//
+//  Created by Alun You on 7/3/13.
+//  Copyright Alun You 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
